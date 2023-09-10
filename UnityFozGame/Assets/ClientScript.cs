dda80392336b384fc13e2247d019be3a8bc4f1f2
Felipe Luiz Wlodkowski <wlodfelipe@gmail.com>
2023-09-10 20:33:23 -0300

    Drag and drop client
    nesses testes os clientes vão ser movidos por drag and drop pra facilitar temporariamente.

--- UnityFozGame/Assets/ClientScript.cs
+++ UnityFozGame/Assets/ClientScript.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class ClientScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 offset;
+    Collider2D collider2d;
+    public string destinationTag = "DropArea";
+
+    void Awake()
     {
-        
+        collider2d = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        offset = transform.position - MouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        transform.position = MouseWorldPosition() + offset;
+    }
+
+    void OnMouseUp()
+    {
+        collider2d.enabled = false;
+        var rayOrigin = Camera.main.transform.position;
+        var rayDirection = MouseWorldPosition() - Camera.main.transform.position;
+        RaycastHit2D hitInfo;
+        if (hitInfo = Physics2D.Raycast(rayOrigin, rayDirection))
+        {
+            if (hitInfo.transform.tag == destinationTag)
+            {
+                transform.position = hitInfo.transform.position + new Vector3(0, 0, -0.01f);
+            }
+        }
+        collider2d.enabled = true;
+    }
+
+    Vector3 MouseWorldPosition()
+    {
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPos);
     }
 }
